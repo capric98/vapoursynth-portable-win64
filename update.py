fa8a5ad5946456f7fb690b3d0c4ab2a7ea7861f0
capric98 <42015599+capric98@users.noreply.github.com>
2025-05-07 00:48:28 -0500

    update iss logic

--- update.py
+++ update.py
@@ -3,24 +3,23 @@
 import re
 import requests
 
-def get_latest_py(tags: list[str], major_ver: str) -> str:
+def get_latest_py(tags: list[str], version: str) -> str:
     max_minor = -1
 
     for tag in tags:
-        if re.match(f"v{major_ver}.[0-9]+$", tag):
+        if re.match(f"v{version}.[0-9]+$", tag):
             minor = int(re.search(r"(?<=.)[0-9]+$", tag).group(0))
             max_minor = max(minor, max_minor)
 
-    if max_minor == -1:
-        raise ValueError("no minor version")
-
-    return f"{major_ver}.{max_minor}"
+    return f"{version}.{max_minor}" if max_minor!=-1 else ""
 
 if __name__=="__main__":
     resp = requests.get("https://api.github.com/repos/vapoursynth/vapoursynth/releases")
     resp.raise_for_status()
     releases = resp.json()
     vs_version = releases[0]["name"]
+
+    print(f"VapourSynth version: \"{vs_version}\"")
 
     resp = requests.get("https://api.github.com/repos/vapoursynth/vapoursynth/tags")
     resp.raise_for_status()
@@ -36,16 +35,26 @@ if __name__=="__main__":
         "/installer/vsinstaller.iss"
     )
     resp.raise_for_status()
-    py_major_version = re.search(r"(?<=PythonVersion ')[.0-9]+", resp.text).group(0)
+
+    try:
+        py_minor_version_low  = int(re.search(r"(?<=PythonVersionMinorLow )[0-9]+", resp.text).group(0))
+        py_minor_version_high = int(re.search(r"(?<=PythonVersionMinorHigh )[0-9]+", resp.text).group(0))
+    except AttributeError as e:
+        print(e)
+        print(resp.text)
 
     resp = requests.get("https://api.github.com/repos/python/cpython/tags")
     resp.raise_for_status()
     tags = resp.json()
 
-    py_version = get_latest_py(
-        tags = [tag["name"] for tag in tags],
-        major_ver = py_major_version,
-    )
+    for py_minor_version in range(py_minor_version_high, py_minor_version_low, -1):
+        py_version = get_latest_py(
+            tags = [tag["name"] for tag in tags],
+            version = f"3.{py_minor_version}",
+        )
+        if py_version:
+            print(f"Python {py_version} selected.")
+            break
 
     with open("config/VapourSynth.txt", mode="w", encoding="utf-8") as f:
         print(vs_version, end=None, file=f)
